c171fdb836e971bf60c2bbd04575e43d753dc928
Denis Timofeenko <rentaltech@yandex.ru>
2025-07-29 21:41:58 +0300

    top10bug

--- src/main.js
+++ src/main.js
@@ -108,7 +108,7 @@ function analyzeSalesData(data, options) {
     seller.bonus = calculateBonusByProfit(index, total, seller)
     // Формируем топ-10 товаров
     seller.top_products = Object.entries(seller.products_sold)
-      .sort((a, b) => b[1] - a[1])
+      .sort((a, b) => b - a)
       .slice(0, 10)
       .map(([sku, quantity]) => ({
         sku: sku,
