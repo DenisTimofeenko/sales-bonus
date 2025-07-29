06bb41f7076e77fbac3d8e1c830164f8135f54e9
Denis Timofeenko <rentaltech@yandex.ru>
2025-07-29 20:51:48 +0300

    conditions

--- src/main.js
+++ src/main.js
@@ -45,6 +45,15 @@ function analyzeSalesData(data, options) {
   if (!data || !Array.isArray(data.sellers) || data.sellers.length === 0) {
     throw new Error("Некорректные входные данные")
   }
+   if (!data || !Array.isArray(data.customers) || data.customers.length === 0) {
+    throw new Error("Некорректные входные данные")
+  }
+  if (!data || !Array.isArray(data.products) || data.products.length === 0) {
+    throw new Error("Некорректные входные данные")
+  }
+  if (!data || !Array.isArray(data.purchase_records) || data.purchase_records.length === 0) {
+    throw new Error("Некорректные входные данные")
+  }
   // @TODO: Проверка наличия опций
   if (
     !options.calculateRevenue ||
